e08f1ef6e5923028f842fb49fd24474ae7082121
Ryan Haraki <54621020+RyanHaraki@users.noreply.github.com>
2025-06-26 21:31:12 -0700

    959fe0c9

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -79,7 +79,7 @@ const Signup = () => {
                   required
                   value={formData.name}
                   onChange={handleInputChange}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="appearance-none block text-black w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   placeholder="John Doe"
                 />
               </div>
